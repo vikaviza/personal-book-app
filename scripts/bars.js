69c30bcac1976ea536c9ba6092c415f322488faa
vikaviza <vikce876@gmail.com>
2023-04-17 21:47:23 +0300

    Task rework

--- scripts/bars.js
+++ scripts/bars.js
@@ -1,12 +1,6 @@
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
-bookBlock = document.getElementById('bookBlock');
-authorBlock = document.getElementById('authorBlock');
-priceBlock = document.getElementById('priceBlock');
 
 burger.addEventListener("click", function(){
     nav.classList.toggle("translate");
-    bookBlock.classList.remove("display");
-    authorBlock.classList.remove("display");
-    priceBlock.classList.remove("display");
 })
